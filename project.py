699ebe039c7c854580a7cbaf80599385f7075f39
Daria Savtšenko <dariasav@ut.ee>
2023-11-16 14:35:29 +0200

    Programmi algversioon

--- project.py
+++ project.py
@@ -1,17 +1,18 @@
 import tkinter as tk
 from tkcalendar import Calendar
 from datetime import datetime
-
-###
 import json
+import uuid
+
 
 def ülesanne_lisamine():
     valitud_kuupäev = kalender.get_date()
     ülesande_tekst = sisestatud_tekst.get()
+    task_id = str(uuid.uuid4())
 
     # märkeruudu lisamine
 
-    märkeruut = tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(märkeruut, valitud_kuupäev, ülesande_tekst))
+    märkeruut = tk.Checkbutton(ülesannete_kuvamine, command=lambda t_id=task_id, date=valitud_kuupäev, text=ülesande_tekst: märki_ülesanne_täidetuks(t_id, date, text))
 
     ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
 
@@ -20,20 +21,24 @@ def ülesanne_lisamine():
     ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
     
     ### Сохранение задачи в файл
-    save_task({"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst})
+    ülesanne = {"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst}
+    ülesanne_salvestamine(ülesanne)
 
     # sisestusvälja puhastamine
     sisestatud_tekst.delete(0, tk.END)
 
-def märki_ülesanne_täidetuks(märkeruut, valitud_kuupäev, ülesande_tekst):
+def märkeruutu_loomine(task_id, valitud_kuupäev, ülesande_tekst):
+    return tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst))
+
+def märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst):
 
     # see kriipsutab vastava ülesande üle, kui märkeruut on märgitud
     ülesande_indeks = ülesannete_kuvamine.index(tk.CURRENT)
     ülesannete_kuvamine.tag_add("tehtud", f"{ülesande_indeks} linestart", f"{ülesande_indeks} lineend")
     ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
 
-    ### Удаление выполненной задачи из файла
-    delete_completed_tasks(valitud_kuupäev, ülesande_tekst)
+    lõpetatud_ülesannete_kustutamine(task_id)
+
 
 def uuenda_ülesannete_kuvamist(sündmus):
     #praegu puhastame ülesannete kuvamine välja uue päevavalimisel
@@ -43,54 +48,64 @@ def uuenda_ülesannete_kuvamist(sündmus):
     # ja kuvada need tekstiväljal
     
     ### Загрузка задач из файла
-    tasks = load_tasks(valitud_kuupäev)
+    ülesanded = ülesannete_laadimine(valitud_kuupäev)
     
-    for task in tasks:
-        ülesanne_koos_märkeruuduga = f"{task['date']}: {task['text']}\n"
+    for ülesanne in ülesanded:
+        märkeruut = märkeruutu_loomine(ülesanne.get('id'), ülesanne['date'], ülesanne['text'])
+        ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
+        ülesanne_koos_märkeruuduga = f"{ülesanne['date']}: {ülesanne['text']}\n"
         ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
 
-def save_task(task):
+        if ülesanne.get("completed", False):
+            ülesanne_indeks = ülesannete_kuvamine.index(tk.END)
+            ülesannete_kuvamine.tag_add("tehtud", f"{ülesanne_indeks} linestart", f"{ülesanne_indeks} lineend")
+            ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
+
+
+
+def ülesanne_salvestamine(ülesanne):
     ### Сохранение задачи в файл
     with open("tasks.json", "a") as file:
-        json.dump(task, file)
+        json.dump(ülesanne, file)
         file.write('\n')
 
-def load_tasks(date):
-    tasks = []
+def ülesannete_laadimine(date):
+    ülesanded = []
     try:
         ### Загрузка задач из файла
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                if task['date'] == date:
-                    tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                if ülesanne['date'] == date:
+                    ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
-def delete_completed_tasks(task_id):
+def lõpetatud_ülesannete_kustutamine(task_id):
     ### Удаление выполненной задачи из файла
-    tasks = load_all_tasks()
-    updated_tasks = [task for task in tasks if task['id'] != task_id]
+    ülesanded = kõikide_ülesannete_laadimine()
+    uuendatud_ülesanded = [ülesanne for ülesanne in ülesanded if ülesanne.get('id') != task_id]
 
-    with open("tasks.json", "w") as file:
-        for task in updated_tasks:
-            json.dump(task, file)
-            file.write('\n')
 
-def load_all_tasks():
+    with open("tasks.json", "w") as fail:
+        for ülesanne in uuendatud_ülesanded:
+            json.dump(ülesanne, fail)
+            fail.write('\n')
+
+def kõikide_ülesannete_laadimine():
     ### Загрузка всех задач из файла
-    tasks = []
+    ülesanded = []
     try:
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
 täna = datetime.today()
 juur = tk.Tk()
@@ -115,4 +130,3 @@ lisa_nupp.grid(row=1, column=2, pady=5)
 #Nupp ülesande lisamiseks
 kalender.bind("<<CalendarSelected>>", uuenda_ülesannete_kuvamist)
 juur.mainloop()
-
