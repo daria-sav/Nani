ac7596c5186e51827b91b765abd54be5ed3f543c
Elisabeth Serikova <you@example.com>
2023-12-21 16:04:30 +0200

    the end

--- no_button_back.py
+++ no_button_back.py
@@ -1,160 +1,242 @@
 import tkinter as tk
 from tkcalendar import Calendar
+from tkinter import filedialog
 from datetime import datetime
+from tkinter import scrolledtext
+from PIL import Image, ImageTk
 import json
-
-# Global variables
-checkbuttons = []
-checkbutton_states = {}
-
-def load_existing_tasks():
-    tasks = load_all_tasks()
-    for task in tasks:
-        valitud_kuupäev = task['date']
-        ülesande_tekst = task['text']
-        task_id = task['id']
-
-        # Recreate Checkbuttons for existing tasks
-        check_var = tk.BooleanVar(value=checkbutton_states.get(task_id, False))
-        märkeruut = tk.Checkbutton(ülesannete_kuvamine, variable=check_var,
-                                   command=lambda var=check_var, id=task_id, date=valitud_kuupäev, text=ülesande_tekst:
-                                   märki_ülesanne_täidetuks(var, id, date, text))
-        ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
-
-        # Update Checkbuttons' states
-        checkbuttons.append((märkeruut, check_var))
+import uuid
 
 def ülesanne_lisamine():
     valitud_kuupäev = kalender.get_date()
     ülesande_tekst = sisestatud_tekst.get()
+    task_id = str(uuid.uuid4())
 
-    # Generate a unique task_id
-    task_id = int(datetime.timestamp(datetime.now()) * 1000)
-
-    # Checkbox creation
-    check_var = tk.BooleanVar()
-    märkeruut = tk.Checkbutton(ülesannete_kuvamine, variable=check_var,
-                               command=lambda var=check_var, id=task_id, date=valitud_kuupäev, text=ülesande_tekst:
-                               märki_ülesanne_täidetuks(var, id, date, text))
+    # Märkeruudu loomine
+    märkeruut = tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst))
     ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
 
-    # Store a reference to the Checkbutton and its associated variable
-    checkbuttons.append((märkeruut, check_var))
-
-    # Update Checkbuttons' states
-    check_var.set(checkbutton_states.get(task_id, False))
-
-    # Task addition to the text field
+    # Lisamine uudise tekstiväljale
     ülesanne_koos_märkeruuduga = f"{valitud_kuupäev}: {ülesande_tekst}\n"
     ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
+    
+    # Salvestamine ülesandefaili
+    ülesanne = {"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst}
+    ülesanne_salvestamine(ülesanne)
 
-    # Save the task to the file
-    save_task({"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst})
+    # Sisestusvälja tühjendamine
+    sisestatud_tekst.delete(0, tk.END) 
 
-    # Store Checkbutton state
-    checkbutton_states[task_id] = check_var.get()
+def copy_text():
+    selected_text = ülesannete_kuvamine.get(tk.SEL_FIRST, tk.SEL_LAST)
+    if selected_text:
+        juur.clipboard_clear()
+        juur.clipboard_append(selected_text)
 
-    # Clear the input field
-    sisestatud_tekst.delete(0, tk.END)
+def paste_text():
+    try:
+        pasted_text = juur.clipboard_get()
+        sisestatud_tekst.insert(tk.END, pasted_text)
+    except tk.TclError:
+        pass
 
-def märki_ülesanne_täidetuks(check_var, task_id, valitud_kuupäev, ülesande_tekst):
-    # Update Checkbuttons' states
-    checkbutton_states[task_id] = check_var.get()
+# Hotkeys
+def on_key_press(event):
+    if märkmik == juur.focus_get() and event.keysym == 'Return':
+        salvesta_märkmik()
+    elif event.keysym.lower() == 'c' and event.state == 0x4:  # Ctrl+C
+        copy_text()
+    elif event.keysym.lower() == 'v' and event.state == 0x4:  # Ctrl+V
+        paste_text()
+    elif event.keysym == 'Return':
+        ülesanne_lisamine()
 
-    # Cross out the corresponding task when the checkbox is checked
-    ülesanne_indeks = ülesannete_kuvamine.index(tk.CURRENT)
-    ülesannete_kuvamine.tag_add("tehtud", f"{ülesanne_indeks} linestart", f"{ülesanne_indeks} lineend")
+def märkmik_on_enter(event):
+    märkmik.insert(tk.END, '\n')
+    salvesta_märkmik() 
+
+def märkeruutu_loomine(task_id, valitud_kuupäev, ülesande_tekst):
+    return tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst))
+
+def märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst):
+    # Ülesande märkimine tehtuks, kui märkeruut on märgitud
+    ülesande_indeks = ülesannete_kuvamine.index(tk.CURRENT)
+    ülesannete_kuvamine.tag_add("tehtud", f"{ülesande_indeks} linestart", f"{ülesande_indeks} lineend")
     ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
 
-    # Remove the completed task from the file
-    delete_completed_tasks(task_id)
+    # Lõpetatud ülesande eemaldamine failist
+    lõpetatud_ülesannete_kustutamine(task_id)
 
 def uuenda_ülesannete_kuvamist(sündmus):
-    # Clear the text field when selecting a new day
+    # Tekstivälja tühjendamine uue kuupäeva valimisel kalendrist
     ülesannete_kuvamine.delete(1.0, tk.END)
     valitud_kuupäev = kalender.get_date()
-
-    # Load tasks from the file
-    tasks = load_tasks(valitud_kuupäev)
-    for task in tasks:
-        check_var = tk.BooleanVar(value=checkbutton_states.get(task['id'], False))
-        märkeruut = tk.Checkbutton(ülesannete_kuvamine, variable=check_var,
-                                   command=lambda t=task, var=check_var:
-                                   märki_ülesanne_täidetuks(var, t['id'], t['date'], t['text']))
+    
+    # Ülesannete laadimine failist ja nende kuvamine tekstiväljal
+    ülesanded = ülesannete_laadimine(valitud_kuupäev)
+    
+    for ülesanne in ülesanded:
+        märkeruut = märkeruutu_loomine(ülesanne.get('id'), ülesanne['date'], ülesanne['text'])
         ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
-        ülesanne_koos_märkeruuduga = f"{task['date']}: {task['text']}\n"
+        ülesanne_koos_märkeruuduga = f"{ülesanne['date']}: {ülesanne['text']}\n"
         ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
 
-def save_task(task):
-    # Save the task to the file
+        if ülesanne.get("completed", False):
+            ülesanne_indeks = ülesannete_kuvamine.index(tk.END)
+            ülesannete_kuvamine.tag_add("tehtud", f"{ülesanne_indeks} linestart", f"{ülesanne_indeks} lineend")
+            ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
+
+def ülesanne_salvestamine(ülesanne):
+    # Salvesta ülesanne faili
     with open("tasks.json", "a") as file:
-        json.dump(task, file)
+        json.dump(ülesanne, file)
         file.write('\n')
 
-def load_tasks(date):
-    tasks = []
+def ülesannete_laadimine(date):
+    # Lae ülesanded failist
+    ülesanded = []
     try:
-        # Load tasks from the file
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                if task['date'] == date:
-                    tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                if ülesanne['date'] == date:
+                    ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
-def delete_completed_tasks(task_id):
-    # Remove the completed task from the file
-    tasks = load_all_tasks()
-    updated_tasks = [task for task in tasks if task['id'] != task_id]
+def lõpetatud_ülesannete_kustutamine(task_id):
+    # Eemalda lõpetatud ülesanne failist
+    ülesanded = kõikide_ülesannete_laadimine()
+    uuendatud_ülesanded = [ülesanne for ülesanne in ülesanded if ülesanne.get('id') != task_id]
 
-    with open("tasks.json", "w") as file:
-        for task in updated_tasks:
-            json.dump(task, file)
-            file.write('\n')
+    with open("tasks.json", "w") as fail:
+        for ülesanne in uuendatud_ülesanded:
+            json.dump(ülesanne, fail)
+            fail.write('\n')
 
-def load_all_tasks():
-    # Load all tasks from the file
-    tasks = []
+def kõikide_ülesannete_laadimine():
+    # Lae kõik ülesanded failist
+    ülesanded = []
     try:
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
-# Main window initialization
+def salvesta_märkmik():
+    # Salvesta märkmik faili
+    märkmik_text = märkmik.get("1.0", tk.END)
+    with open("märkmik.txt", "w", encoding="utf-8") as file:
+        file.write(märkmik_text)
+
+def laadi_märkmik():
+    # Lae märkmik failist
+    try:
+        with open("märkmik.txt", "r", encoding="utf-8") as file:
+            märkmik_text = file.read()
+            märkmik.delete(1.0, tk.END)
+            märkmik.insert(tk.END, märkmik_text)
+    except FileNotFoundError:
+        pass
+
+def toggle_media():
+    if media_button.winfo_ismapped():
+        media_button.grid_forget()
+        show_selected_media()
+    else:
+        hide_selected_media()
+        media_button.grid(row=4, column=3, padx=10, pady=10, sticky="nsew")
+
+def show_selected_media():
+    media_canvas.place(relx=0, rely=0, anchor=tk.NW)
+
+def hide_selected_media():
+    media_canvas.place_forget()
+
+# Funktsioon pildi valimiseks
+def vali_media_fail():
+    # Ava failivaliku dialoog meediale
+    file_path = filedialog.askopenfilename()
+
+    # Näita valitud pilti või GIFi lõuendil
+    if file_path:
+        # Lae pilt
+        original_image = Image.open(file_path)
+
+        # Saada suurused
+        width, height = original_image.size
+
+        # Saada suurused, mis on saadaval pildi jaoks
+        max_width = ülesannete_kuvamine.winfo_width()  # Uudise laius
+        max_height = media_canvas.winfo_height()  # Kalendri uudise kõrgus
+
+        # Arvuta uued pildi suurused
+        new_width = max_width
+        new_height = int(height * (max_width / width))
+
+        # Kui uus kõrgus on suurem kui maksimaalne kõrgus, arvuta uuesti suurused
+        if new_height > max_height:
+            new_height = max_height
+            new_width = int(width * (max_height / height))
+
+        # Muuda pildi suurust
+        resized_image = original_image.resize((new_width, new_height), Image.LANCZOS)
+
+        # Puhasta lõuend enne uue pildi kuvamist
+        media_canvas.delete("all")
+
+        # Teisenda pilt PhotoImage-iks lõuendile kuvamiseks
+        img_tk = ImageTk.PhotoImage(resized_image)
+
+        # Joonista pilt lõuendile
+        media_canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
+        media_canvas.image = img_tk
+
+# Põhiaken
 täna = datetime.today()
 juur = tk.Tk()
 juur.title("Kalender ülesannetega")
 
-# Calendar creation
+# Kalender
 kalender = Calendar(juur, selectmode='day', year=täna.year, month=täna.month, day=täna.day)
-kalender.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
+kalender.grid(row=0, column=0, padx=10, pady=10, columnspan=3, sticky="nsew")
 
-# Text field for entering tasks
+# Sisestusväli ülesande jaoks
 sisestatud_tekst = tk.Entry(juur, width=30)
-sisestatud_tekst.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+sisestatud_tekst.grid(row=1, column=0, padx=10, pady=10, columnspan=2, sticky="nsew")
 
-# Button for adding a task
+# Nupp ülesande lisamiseks
 lisa_nupp = tk.Button(juur, text="Lisa ülesanne", command=ülesanne_lisamine)
-lisa_nupp.grid(row=1, column=2, pady=5)
+lisa_nupp.grid(row=1, column=2, pady=5, sticky="nsew")
 
-# Text field for displaying tasks
+# Tekstiväli ülesannete kuvamiseks
 ülesannete_kuvamine = tk.Text(juur, height=10, width=30)
-ülesannete_kuvamine.grid(row=2, column=0, padx=10, pady=10, columnspan=3)
+ülesannete_kuvamine.grid(row=2, column=0, padx=10, pady=10, columnspan=3, sticky="nsew")
 
-# Event when selecting a day in the calendar
+# Sündmus uue kuupäeva valimisel kalendrist
 kalender.bind("<<CalendarSelected>>", uuenda_ülesannete_kuvamist)
 
-# Load existing tasks and recreate Checkbuttons when the application starts
-load_existing_tasks()
+# Bind hotkeys
+juur.bind('<Return>', on_key_press)
+juur.bind_all('<Control-c>', lambda e: on_key_press(e))
+juur.bind_all('<Control-v>', lambda e: on_key_press(e))
 
-# Start the Tkinter main loop
+märkmik = scrolledtext.ScrolledText(juur, wrap=tk.WORD, width=30, height=10)
+märkmik.grid(row=0, column=3, padx=10, pady=10, rowspan=2, sticky="nsew")
+märkmik.bind('<Return>', märkmik_on_enter)
+
+laadi_märkmik()
+
+media_canvas = tk.Canvas(juur, width=150, height=150)
+media_canvas.grid(row=2, column=3, padx=10, pady=10, sticky="nsew")
+
+media_nupp = tk.Button(juur, text="Vali media fail", command=vali_media_fail)
+media_nupp.grid(row=3, column=3, pady=5, sticky="nsew")
+
 juur.mainloop()
-
--- project.py
+++ project.py
@@ -1,116 +1,242 @@
 import tkinter as tk
 from tkcalendar import Calendar
-
-###
+from tkinter import filedialog
+from datetime import datetime
+from tkinter import scrolledtext
+from PIL import Image, ImageTk
 import json
+import uuid
 
 def ülesanne_lisamine():
     valitud_kuupäev = kalender.get_date()
     ülesande_tekst = sisestatud_tekst.get()
+    task_id = str(uuid.uuid4())
 
-    # märkeruudu lisamine
-
-    märkeruut = tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(märkeruut, valitud_kuupäev, ülesande_tekst))
-
+    # Märkeruudu loomine
+    märkeruut = tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst))
     ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
 
-    # ülesanne lisamine tekstivälja
+    # Lisamine uudise tekstiväljale
     ülesanne_koos_märkeruuduga = f"{valitud_kuupäev}: {ülesande_tekst}\n"
     ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
     
-    ### Сохранение задачи в файл
-    save_task({"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst})
+    # Salvestamine ülesandefaili
+    ülesanne = {"id": task_id, "date": valitud_kuupäev, "text": ülesande_tekst}
+    ülesanne_salvestamine(ülesanne)
 
-    # sisestusvälja puhastamine
-    sisestatud_tekst.delete(0, tk.END)
+    # Sisestusvälja tühjendamine
+    sisestatud_tekst.delete(0, tk.END) 
 
-def märki_ülesanne_täidetuks(märkeruut, valitud_kuupäev, ülesande_tekst):
+def copy_text():
+    selected_text = ülesannete_kuvamine.get(tk.SEL_FIRST, tk.SEL_LAST)
+    if selected_text:
+        juur.clipboard_clear()
+        juur.clipboard_append(selected_text)
 
-    # see kriipsutab vastava ülesande üle, kui märkeruut on märgitud
+def paste_text():
+    try:
+        pasted_text = juur.clipboard_get()
+        sisestatud_tekst.insert(tk.END, pasted_text)
+    except tk.TclError:
+        pass
+
+# Hotkeys
+def on_key_press(event):
+    if märkmik == juur.focus_get() and event.keysym == 'Return':
+        salvesta_märkmik()
+    elif event.keysym.lower() == 'c' and event.state == 0x4:  # Ctrl+C
+        copy_text()
+    elif event.keysym.lower() == 'v' and event.state == 0x4:  # Ctrl+V
+        paste_text()
+    elif event.keysym == 'Return':
+        ülesanne_lisamine()
+
+def märkmik_on_enter(event):
+    märkmik.insert(tk.END, '\n')
+    salvesta_märkmik() 
+
+def märkeruutu_loomine(task_id, valitud_kuupäev, ülesande_tekst):
+    return tk.Checkbutton(ülesannete_kuvamine, command=lambda: märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst))
+
+def märki_ülesanne_täidetuks(task_id, valitud_kuupäev, ülesande_tekst):
+    # Ülesande märkimine tehtuks, kui märkeruut on märgitud
     ülesande_indeks = ülesannete_kuvamine.index(tk.CURRENT)
     ülesannete_kuvamine.tag_add("tehtud", f"{ülesande_indeks} linestart", f"{ülesande_indeks} lineend")
     ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
 
-    ### Удаление выполненной задачи из файла
-    delete_completed_tasks(valitud_kuupäev, ülesande_tekst)
+    # Lõpetatud ülesande eemaldamine failist
+    lõpetatud_ülesannete_kustutamine(task_id)
 
 def uuenda_ülesannete_kuvamist(sündmus):
-    #praegu puhastame ülesannete kuvamine välja uue päevavalimisel
+    # Tekstivälja tühjendamine uue kuupäeva valimisel kalendrist
     ülesannete_kuvamine.delete(1.0, tk.END)
     valitud_kuupäev = kalender.get_date()
-    # siit pärast saame laadida valitud päeva ülesanded failist
-    # ja kuvada need tekstiväljal
     
-    ### Загрузка задач из файла
-    tasks = load_tasks(valitud_kuupäev)
+    # Ülesannete laadimine failist ja nende kuvamine tekstiväljal
+    ülesanded = ülesannete_laadimine(valitud_kuupäev)
     
-    for task in tasks:
-        ülesanne_koos_märkeruuduga = f"{task['date']}: {task['text']}\n"
+    for ülesanne in ülesanded:
+        märkeruut = märkeruutu_loomine(ülesanne.get('id'), ülesanne['date'], ülesanne['text'])
+        ülesannete_kuvamine.window_create(tk.END, window=märkeruut)
+        ülesanne_koos_märkeruuduga = f"{ülesanne['date']}: {ülesanne['text']}\n"
         ülesannete_kuvamine.insert(tk.END, ülesanne_koos_märkeruuduga)
 
-def save_task(task):
-    ### Сохранение задачи в файл
+        if ülesanne.get("completed", False):
+            ülesanne_indeks = ülesannete_kuvamine.index(tk.END)
+            ülesannete_kuvamine.tag_add("tehtud", f"{ülesanne_indeks} linestart", f"{ülesanne_indeks} lineend")
+            ülesannete_kuvamine.tag_configure("tehtud", overstrike=True)
+
+def ülesanne_salvestamine(ülesanne):
+    # Salvesta ülesanne faili
     with open("tasks.json", "a") as file:
-        json.dump(task, file)
+        json.dump(ülesanne, file)
         file.write('\n')
 
-def load_tasks(date):
-    tasks = []
+def ülesannete_laadimine(date):
+    # Lae ülesanded failist
+    ülesanded = []
     try:
-        ### Загрузка задач из файла
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                if task['date'] == date:
-                    tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                if ülesanne['date'] == date:
+                    ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
-def delete_completed_tasks(task_id):
-    ### Удаление выполненной задачи из файла
-    tasks = load_all_tasks()
-    updated_tasks = [task for task in tasks if task['id'] != task_id]
+def lõpetatud_ülesannete_kustutamine(task_id):
+    # Eemalda lõpetatud ülesanne failist
+    ülesanded = kõikide_ülesannete_laadimine()
+    uuendatud_ülesanded = [ülesanne for ülesanne in ülesanded if ülesanne.get('id') != task_id]
 
-    with open("tasks.json", "w") as file:
-        for task in updated_tasks:
-            json.dump(task, file)
-            file.write('\n')
+    with open("tasks.json", "w") as fail:
+        for ülesanne in uuendatud_ülesanded:
+            json.dump(ülesanne, fail)
+            fail.write('\n')
 
-def load_all_tasks():
-    ### Загрузка всех задач из файла
-    tasks = []
+def kõikide_ülesannete_laadimine():
+    # Lae kõik ülesanded failist
+    ülesanded = []
     try:
-        with open("tasks.json", "r") as file:
-            for line in file:
-                task = json.loads(line.strip())
-                tasks.append(task)
+        with open("tasks.json", "r") as fail:
+            for rida in fail:
+                ülesanne = json.loads(rida.strip())
+                ülesanded.append(ülesanne)
     except FileNotFoundError:
         pass
 
-    return tasks
+    return ülesanded
 
+def salvesta_märkmik():
+    # Salvesta märkmik faili
+    märkmik_text = märkmik.get("1.0", tk.END)
+    with open("märkmik.txt", "w", encoding="utf-8") as file:
+        file.write(märkmik_text)
+
+def laadi_märkmik():
+    # Lae märkmik failist
+    try:
+        with open("märkmik.txt", "r", encoding="utf-8") as file:
+            märkmik_text = file.read()
+            märkmik.delete(1.0, tk.END)
+            märkmik.insert(tk.END, märkmik_text)
+    except FileNotFoundError:
+        pass
+
+def toggle_media():
+    if media_button.winfo_ismapped():
+        media_button.grid_forget()
+        show_selected_media()
+    else:
+        hide_selected_media()
+        media_button.grid(row=4, column=3, padx=10, pady=10, sticky="nsew")
+
+def show_selected_media():
+    media_canvas.place(relx=0, rely=0, anchor=tk.NW)
+
+def hide_selected_media():
+    media_canvas.place_forget()
+
+# Funktsioon pildi valimiseks
+def vali_media_fail():
+    # Ava failivaliku dialoog meediale
+    file_path = filedialog.askopenfilename()
+
+    # Näita valitud pilti või GIFi lõuendil
+    if file_path:
+        # Lae pilt
+        original_image = Image.open(file_path)
+
+        # Saada suurused
+        width, height = original_image.size
+
+        # Saada suurused, mis on saadaval pildi jaoks
+        max_width = ülesannete_kuvamine.winfo_width()  # Uudise laius
+        max_height = media_canvas.winfo_height()  # Kalendri uudise kõrgus
+
+        # Arvuta uued pildi suurused
+        new_width = max_width
+        new_height = int(height * (max_width / width))
+
+        # Kui uus kõrgus on suurem kui maksimaalne kõrgus, arvuta uuesti suurused
+        if new_height > max_height:
+            new_height = max_height
+            new_width = int(width * (max_height / height))
+
+        # Muuda pildi suurust
+        resized_image = original_image.resize((new_width, new_height), Image.LANCZOS)
+
+        # Puhasta lõuend enne uue pildi kuvamist
+        media_canvas.delete("all")
+
+        # Teisenda pilt PhotoImage-iks lõuendile kuvamiseks
+        img_tk = ImageTk.PhotoImage(resized_image)
+
+        # Joonista pilt lõuendile
+        media_canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
+        media_canvas.image = img_tk
+
+# Põhiaken
+täna = datetime.today()
 juur = tk.Tk()
 juur.title("Kalender ülesannetega")
 
-# Kalendri loomine
-kalender = Calendar(juur, selectmode='day', year=2023, month=11, day=9)
-kalender.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
+# Kalender
+kalender = Calendar(juur, selectmode='day', year=täna.year, month=täna.month, day=täna.day)
+kalender.grid(row=0, column=0, padx=10, pady=10, columnspan=3, sticky="nsew")
 
-
+# Sisestusväli ülesande jaoks
 sisestatud_tekst = tk.Entry(juur, width=30)
-sisestatud_tekst.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+sisestatud_tekst.grid(row=1, column=0, padx=10, pady=10, columnspan=2, sticky="nsew")
 
-# Sisestusvälja loomine ülesande jaoks
+# Nupp ülesande lisamiseks
 lisa_nupp = tk.Button(juur, text="Lisa ülesanne", command=ülesanne_lisamine)
-lisa_nupp.grid(row=1, column=2, pady=5)
+lisa_nupp.grid(row=1, column=2, pady=5, sticky="nsew")
 
-#Nupp ülesande lisamiseks
+# Tekstiväli ülesannete kuvamiseks
 ülesannete_kuvamine = tk.Text(juur, height=10, width=30)
-ülesannete_kuvamine.grid(row=2, column=0, padx=10, pady=10, columnspan=3)
+ülesannete_kuvamine.grid(row=2, column=0, padx=10, pady=10, columnspan=3, sticky="nsew")
 
-#Nupp ülesande lisamiseks
+# Sündmus uue kuupäeva valimisel kalendrist
 kalender.bind("<<CalendarSelected>>", uuenda_ülesannete_kuvamist)
-juur.mainloop()
 
+# Bind hotkeys
+juur.bind('<Return>', on_key_press)
+juur.bind_all('<Control-c>', lambda e: on_key_press(e))
+juur.bind_all('<Control-v>', lambda e: on_key_press(e))
+
+märkmik = scrolledtext.ScrolledText(juur, wrap=tk.WORD, width=30, height=10)
+märkmik.grid(row=0, column=3, padx=10, pady=10, rowspan=2, sticky="nsew")
+märkmik.bind('<Return>', märkmik_on_enter)
+
+laadi_märkmik()
+
+media_canvas = tk.Canvas(juur, width=150, height=150)
+media_canvas.grid(row=2, column=3, padx=10, pady=10, sticky="nsew")
+
+media_nupp = tk.Button(juur, text="Vali media fail", command=vali_media_fail)
+media_nupp.grid(row=3, column=3, pady=5, sticky="nsew")
+
+juur.mainloop()
